765e2a240f06a7e10d7feed268680297a902b063
Tyler Weir <tyler.weir@gmail.com>
2009-07-05 10:30:15 -0400

    Seed repo

---
+++ makeapp.sh
@@ -0,0 +1,9 @@
+mvn archetype:generate -U \
+-DarchetypeGroupId=net.liftweb \
+-DarchetypeArtifactId=lift-archetype-basic \
+-DarchetypeVersion=1.0 \
+-DremoteRepositories=http://scala-tools.org/repo-releases \
+-DgroupId=ca.tylerweir \
+-DartifactId=shortbus \
+-Dversion=0.1-SNAPSHOT 
+
---
+++ shortbus/src/main/scala/ca/tylerweir/snippet/HelloWorld.scala
@@ -0,0 +1,6 @@
+package ca.tylerweir.snippet
+
+class HelloWorld {
+  def howdy = <span>Welcome to shortbus at {new _root_.java.util.Date}</span>
+}
+
